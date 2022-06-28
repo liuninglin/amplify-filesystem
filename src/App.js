7c93eba1cf37fb754c805f7e858e13821ba60923
johnny.liu <johnny.liu@connext.com.cn>
2022-06-28 15:47:06 -0400

    add auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
+import React, { useState } from "react";
+import { Amplify } from "aws-amplify";
+import { AmplifyProvider, Authenticator, Image, useTheme, View } from "@aws-amplify/ui-react";
+import { studioTheme } from './ui-components';
+import awsconfig from "./aws-exports";
 import logo from './logo.svg';
+import "@aws-amplify/ui-react/styles.css";
 import './App.css';
 
+Amplify.configure({
+  ...awsconfig,
+});
+
 function App() {
+  const components = {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="Amplidoc logo"
+            src="logo192.png"
+            height="150px"
+          />
+        </View>
+      )
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyProvider theme={studioTheme}>
+      <Authenticator variation="modal" components={components}>
+
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              My App
+            </p>
+          </header>
+        </div>
+
+      </Authenticator>
+    </AmplifyProvider>
+
   );
 }
 
